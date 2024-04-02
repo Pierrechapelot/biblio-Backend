bf5a4048f69cb0ddc667d4f3a51596316a68e7d5
Pierre CHAPELOT <115943334+Pierrechapelot@users.noreply.github.com>
2024-04-02 17:13:54 +0200

    loans schema done

---
+++ models/loans.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const loansSchema = mongoose.Schema({
+  book: { type: mongoose.Schema.Types.ObjectId, ref: "books" },
+  userIdentifier: String,
+  loanDate: Date,
+  dueDate: Date,
+  returnDate: Date,
+  status: String
+//   "loaned", "returned", "late"
+});
+
+const Loan = mongoose.model("loans", loansSchema);
+module.exports = Loan;

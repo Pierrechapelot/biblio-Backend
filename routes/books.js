5d86aa134c098af4ddc3dbdc0d4f21d6bbdd43ec
Pierre CHAPELOT <115943334+Pierrechapelot@users.noreply.github.com>
2024-04-03 15:49:40 +0200

    Check existing authors with regex

--- routes/books.js
+++ routes/books.js
@@ -3,38 +3,56 @@ var router = express.Router();
 
 // const fetch = require("node-fetch");
 const Book = require("../models/books");
-const Author = require("../models/authors")
+const Author = require("../models/authors");
 
-router.post('/', async (req, res) => {
-    const { title, genre, isbn, publicationDate, description, available, firstName, lastName } = req.body;
-  
-    try {
-      // Vérifier si l'auteur existe déjà
-      let author = await Author.findOne({ firstName, lastName });
-  
-      // Si l'auteur n'existe pas, le créer
-      if (!author) {
-        author = new Author({ firstName, lastName });
-        await author.save();
-      }
-  
-      // Maintenant que l'auteur est garanti d'exister, créer le livre
-      const book = new Book({
-        title,
-        author: author._id, // Utiliser l'ID de l'auteur existant ou nouvellement créé
-        genre,
-        isbn,
-        publicationDate,
-        description,
-        available,
-      });
-  
-      const newBook = await book.save();
-      res.status(201).json(newBook);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+// Fonction pour normaliser les prenoms et noms d'auteurs
+function normalizeName(name) {
+  return name
+    .toLowerCase()
+    .trim()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+}
+
+router.post("/", async (req, res) => {
+  const { title, genre, isbn, publicationDate, description, available } =
+    req.body;
+  let { firstName, lastName } = req.body;
+
+  //   On normalise les noms d'auteurs
+  const normalizedFirstName = normalizeName(firstName);
+  const normalizedLastName = normalizeName(lastName);
+
+  try {
+    // Vérifier si l'auteur existe déjà avec les noms normalisés
+    let author = await Author.findOne({
+      firstName: { $regex: new RegExp("^" + normalizedFirstName + "$", "i") },
+      lastName: { $regex: new RegExp("^" + normalizedLastName + "$", "i") },
+    });
+
+    // Si l'auteur n'existe pas, le créer
+    if (!author) {
+      author = new Author({ firstName, lastName });
+      await author.save();
     }
-  });
+
+    // Maintenant que l'auteur est garanti d'exister, créer le livre
+    const book = new Book({
+      title,
+      author: author._id, // Utiliser l'ID de l'auteur existant ou nouvellement créé
+      genre,
+      isbn,
+      publicationDate,
+      description,
+      available,
+    });
+
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // router.post("/", async (req, res) => {
 //   const { isbn } = req.body;
